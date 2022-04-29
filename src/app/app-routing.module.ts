8910466abb20675f8bb406ca54c4445ff7a9fc7a
mksas00 <beqs171@gmail.com>
2022-04-29 16:09:49 +0200

    done 100%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {Routes } from '@angular/router';
+import {OrdersPRComponent} from "./components/orders-pr/orders-pr.component";
+import {OrdersDetailsPRComponent} from "./components/orders-details-pr/orders-details-pr.component";
+import {RouterModule} from '@angular/router';
+const routes: Routes = [
+  {
+    path: '',
+    component: OrdersPRComponent
+  },
+  {
+    path: 'orders/detail/:id',
+    component: OrdersDetailsPRComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersPRComponent } from './components/orders-pr/orders-pr.component';
+import { OrdersItemPRComponent } from './components/orders-item-pr/orders-item-pr.component';
+import { OrdersDetailsPRComponent } from './components/orders-details-pr/orders-details-pr.component';
+import {PRDataService} from "./services/pr-data.service";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersPRComponent,
+    OrdersItemPRComponent,
+    OrdersDetailsPRComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    PRDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
